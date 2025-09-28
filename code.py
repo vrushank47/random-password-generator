0ff459426ef5cf42ee77cd6e48bd2e5a6e037553
vrushank47 <vrushanksaravade2005@gmail.com>
2025-09-28 13:20:20 +0530

    added a gui

--- code.py
+++ code.py
@@ -12,12 +12,13 @@
 
 import random 
 import string 
+from tkinter import *
 
 def generate_password():
-    length = int (input("enter the desired password length:").strip())
-    include_uppercase= input ("include uppercase lettes (yes/no):").lower().strip()
-    include_splcharacters = input ("include these special characters (yes/no):").lower().strip()
-    include_digits = input ("include these digits(yes/no):").lower().strip()
+    length = int (entry_length.get().strip())
+    include_uppercase= var_uppercase.get()
+    include_splcharacters = var_special.get()
+    include_digits = var_digits.get()
 
 
 
@@ -28,19 +29,19 @@ def generate_password():
     
 
     lower = string.ascii_lowercase
-    upper = string.ascii_uppercase if include_uppercase == "yes" else ""
-    digits = string.digits if include_digits == "yes" else ""
-    special = string.punctuation if include_splcharacters == "yes" else ""
+    upper = string.ascii_uppercase if include_uppercase  else ""
+    digits = string.digits if include_digits else ""
+    special = string.punctuation if include_splcharacters else ""
     all_characters = lower + upper + digits +special
 
 
 
     required_characters= []
-    if include_uppercase == "yes":
+    if include_uppercase:
         required_characters.append(random.choice(upper))
-    if include_splcharacters == "yes":
+    if include_splcharacters:
         required_characters.append(random.choice(special))
-    if include_digits == "yes":
+    if include_digits:
         required_characters.append(random.choice(digits))
     
     
@@ -55,9 +56,35 @@ def generate_password():
     random.shuffle(password)
 
     str_password = "".join(password)
-    return str_password
+    result_label.config(text="generate password :" + str_password)
+    return
+root = Tk()
+root.title("passwrod generator")
+
+# length input
+Label(root , text="password length :").pack()
+entry_length = Entry(root)
+entry_length.pack()
+
+
+# options
+var_uppercase = BooleanVar()
+Checkbutton(root , text="include uppercase", variable=var_uppercase).pack()
+
+var_digits = BooleanVar()
+Checkbutton(root , text= "enter digits" , variable=var_digits).pack()
+
+var_special = BooleanVar()
+Checkbutton(root , text="include special characters" , variable=var_special).pack()
+
+
+#generate button
+button = Button(root , text="generate password", command=generate_password).pack()
+
+# result
+result_label = Label(root , text="")
+result_label.pack()
 
 
 
-password = generate_password()
-print(password)
+root.mainloop()
